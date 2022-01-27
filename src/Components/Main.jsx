5474ae5b5f09ec2bdbb13ac3789eeb29d7297468
tabassum2507 <10tabassum2@gmail.com>
2022-01-27 19:36:42 +0530

    refactored

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,41 +1,56 @@
-import React from 'react';
-import './Main.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faShoppingCart, faStarHalf, faChevronRight, faCheck } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Main.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faShoppingCart,
+  faStarHalf,
+  faChevronRight
+} from "@fortawesome/free-solid-svg-icons";
 
 function Main() {
-  return <div className='box'>
-    <span className='box__img'></span>
-    <h1 className='box__heading'>Everyday rinse and reload</h1>
-    <p className='box__detail'>Achieve your dream skin goal with this complete package of skincare produts that will rejuvenate your day.</p>
+  return (
+    <div className="box">
+      <span className="box__img"></span>
+      <h1 className="box__heading">Everyday rinse and reload</h1>
+      <p className="box__detail">
+        Achieve your dream skin goal with this complete package of skincare
+        produts that will rejuvenate your day.
+      </p>
 
-    <div className="section">
-      <div className="section__rating">
-      <FontAwesomeIcon className="star" icon={faStar} />
-      <FontAwesomeIcon className="star" icon={faStar} />
-      <FontAwesomeIcon className="star" icon={faStar} />
-      <FontAwesomeIcon className="star" icon={faStar} />
-      <FontAwesomeIcon className="star" icon={faStarHalf} />
+      <div className="section">
+        <div className="section__rating">
+          <FontAwesomeIcon className="star" icon={faStar} />
+          <FontAwesomeIcon className="star" icon={faStar} />
+          <FontAwesomeIcon className="star" icon={faStar} />
+          <FontAwesomeIcon className="star" icon={faStar} />
+          <FontAwesomeIcon className="star" icon={faStarHalf} />
+        </div>
+        <h3 className="section_num">4.5</h3>
+        <p>
+          Click to read reviews
+          <FontAwesomeIcon icon={faChevronRight} />
+        </p>
       </div>
-      <h3 className='section_num'>4.5</h3>
-      <p>Click to read reviews<FontAwesomeIcon icon={faChevronRight} /></p>
-      
-    </div>
-    
-    <div className='box__money'>
-    <h1>₹899</h1>
-    <h1 className='box__money__h1'>₹1299</h1>
-    </div>
 
-    
+      <div className="box__money">
+        <h1>₹899</h1>
+        <h1 className="box__money__h1">₹1299</h1>
+      </div>
+
       <div className="box__pack1 box__pack1--active">
-        <span className='box__pack1__popular'><FontAwesomeIcon icon={faStar} />MOST POPULAR<FontAwesomeIcon icon={faStar} /></span>
+        <span className="box__pack1__popular">
+          <FontAwesomeIcon icon={faStar} />
+          MOST POPULAR
+          <FontAwesomeIcon icon={faStar} />
+        </span>
 
         <h3>1 Months Pack (4 tubes)</h3>
 
-        <div className='box__pack1__saving'>
+        <div className="box__pack1__saving">
           <span class>Savings: ₹200</span>
-        <h4>₹595</h4></div>
+          <h4>₹595</h4>
+        </div>
 
         <div className="line2">
           <span>38% Saved</span>
@@ -43,30 +58,29 @@ function Main() {
           <h4>₹795</h4>
         </div>
       </div>
-  
-    
-    <div className="box__pack2">
 
-      <h3>3 Months Pack (12 tubes)</h3>
+      <div className="box__pack2">
+        <h3>3 Months Pack (12 tubes)</h3>
 
-      <div className="line1"><span>Savings: ₹100</span>
-      <h3>₹899</h3>
+        <div className="line1">
+          <span>Savings: ₹100</span>
+          <h3>₹899</h3>
+        </div>
+
+        <div className="line2">
+          <p>12% Saved</p>
+          <h3>₹999</h3>
+        </div>
       </div>
 
-      <div 
-      className="line2"><p>12% Saved</p>
-      <h3>₹999</h3>
+      <div className="box__button">
+        <span>
+          <FontAwesomeIcon icon={faShoppingCart} />
+          ADD TO CART
+        </span>
       </div>
-
     </div>
-
-    
-
-    <div className='box__button box__button--box'><span>
-      <FontAwesomeIcon icon={faShoppingCart} />ADD TO CART</span>
-      </div>
-    
-  </div>;
+  );
 }
 
 export default Main;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import "./Navbar.css"
-
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
 
 function Navbar() {
-  return <div className='navbar'>
-    <ul className="navbar__list">
-      <li className='navbar__item'><FontAwesomeIcon icon={faBars} /></li>
-      <li className='navbar__item'><FontAwesomeIcon icon={faShoppingCart} /></li>
-    </ul>
-    <hr />
-      
-      
-
-  </div>;
+  return (
+    <div className="navbar">
+      <ul className="navbar__list">
+        <li >
+          <FontAwesomeIcon icon={faBars} />
+        </li>
+        <li >
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </li>
+      </ul>
+      <hr />
+    </div>
+  );
 }
 
 export default Navbar;
